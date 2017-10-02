200f881d0005cce9e81969723aaadd4b490a57af
Jehoshaphat I. Abu <loveyou725@hotmail.com>
2017-10-02 22:17:53 +0100

    First commit

---
+++ tryCpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    //Defining the variables
+    int age = 0;
+
+    //outputing strings
+    std::cout<<"trying out some cpp"<<"!\n";
+    std::cout<<"how old are you? ";
+
+    //recieving inputs
+    std::cin>>age;
+    std::cout<<"Wow! you look good at "<<age;
+
+    //end part
+    return 0;
+
+}
