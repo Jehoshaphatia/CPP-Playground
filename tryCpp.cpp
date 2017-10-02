65269ca8db6356f38095a3efcb88a04adefb41d1
Jehoshaphat I. Abu <loveyou725@hotmail.com>
2017-10-02 23:04:43 +0100

    Added text file

--- tryCpp.cpp
+++ tryCpp.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
+#include "main.hpp"
 
 int main()
 {
     //Defining the variables
     int age = 0;
+    int yourClass = 0;
 
     //outputing strings
-    std::cout<<"trying out some cpp"<<"!\n";
+    std::cout<<"trying out wewewewsome cpp"<<"!\n";
     std::cout<<"how old are you? ";
+    
 
     //recieving inputs
     std::cin>>age;
     std::cout<<"Wow! you look good at "<<age;
+    std::cout<<"what class are you? ";
+    std::cin>>yourClass;
+    std::cout<<"Wow! You are in class "<<yourClass;
+    
 
     //end part
     return 0;
